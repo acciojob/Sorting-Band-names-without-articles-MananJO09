e3d54d160cbfd72c30c7f308588c82e9e03e67b4
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-22 19:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+function removeArticles(name) {
+	return name.replace(/^(a |an |the )/i, '');
+}
 
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+bandNames.sort((a, b) => {
+    return removeArticles(a).localeCompare(removeArticles(b));
+});
